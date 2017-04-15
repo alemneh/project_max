4b1ef8fc754865a5f58fac44e3ed829c1568bfb2
Alemneh <Alemneh@hotmail.com>
2017-04-15 01:16:42 -0700

    added test for create project view

--- projectmax/api/tests.py
+++ projectmax/api/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from .models import Project
+from rest_framework.test import APIClient
+from rest_framework import status_code
+from django.core.urlresolvers import reverse
 # Create your tests here.
 
 class ModelTestCase(TestCase):
@@ -19,3 +22,19 @@ class ModelTestCase(TestCase):
         self.project.save()
         new_count = Project.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+
+class ViewTestCase(TestCase):
+    """Test suite for api views"""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.client = APIClient()
+        self.response = self.client.post(
+            reverse('create'),
+            self.project_data,
+            format="json")
+
+    def test_api_can_create_a_project(self):
+        """Test the api has project creation capability."""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
--- projectmax/api/views.py
+++ projectmax/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ProjectSerializer
+from .models import Project
 
-# Create your views here.
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of the rest api."""
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new project."""
+        serializer.save()
